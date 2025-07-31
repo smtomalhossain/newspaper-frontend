53ae6d496e8f70085dc1f07f47b4e50529ae93b6
Tomal Hossain <tomalhossain84@gmail.com>
2025-07-31 18:11:56 +0600

    fix next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["www.risingbd.com"],
+    domains: ["www.risingbd.com","cdn.risingbd.com",],
+    
   },
 };
 
